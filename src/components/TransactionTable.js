8bcf7c7d76a07f92ae870d1f7d15a3d017d0bd60
Farida Mutai <faridamutai52@gmail.com>
2024-08-15 12:09:59 +0300

    commented T.T

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,30 +1,36 @@
-
-//TransactionTable.js
 import React from 'react';
 
+// Define the TransactionTable component
 function TransactionTable({ transactions, onDelete, onSort }) {
   return (
     <div>
+      {/* Buttons to sort transactions by different criteria */}
       <button onClick={() => onSort('description')}>Sort by Description</button>
       <button onClick={() => onSort('category')}>Sort by Category</button>
+      
+      {/* Table to display transactions */}
       <table>
         <thead>
           <tr>
-          <th>Date </th>
+            {/* Table headers */}
+            <th>Date</th>
             <th>Description</th>
             <th>Amount</th>
             <th>Category</th>
             <th>Actions</th>
-            </tr>
+          </tr>
         </thead>
         <tbody>
+          {/* Map over the transactions array to create table rows */}
           {transactions.map(transaction => (
             <tr key={transaction.id}>
+              {/* Table data cells */}
               <td>{transaction.date}</td>
               <td>{transaction.description}</td>
               <td>{transaction.amount}</td>
               <td>{transaction.category}</td>
               
+              {/* Action button to delete a transaction */}
               <td>
                 <button onClick={() => onDelete(transaction.id)}>Delete</button>
               </td>
@@ -36,4 +42,5 @@ function TransactionTable({ transactions, onDelete, onSort }) {
   );
 }
 
+// Export the TransactionTable component for use in other parts of the application
 export default TransactionTable;
